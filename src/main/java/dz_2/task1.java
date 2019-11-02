f8d7b3130837ac3e6cd9206ff1c480028fbc4769
MrSmith <hhperm.56@yandex.ru>
2019-11-02 16:12:34 +0500

    done

--- src/main/java/dz_2/task1.java
+++ src/main/java/dz_2/task1.java
@@ -1,4 +1,29 @@
 package dz_2;
 
+import java.util.ArrayList;
+
 public class task1 {
+    public static void main(String[] args) {
+        try{
+            Object npe = null;
+            npe.toString();
+        }
+        catch (Exception e){
+            System.out.println("смоделирован NullPointerException:");
+            e.printStackTrace();
+        }
+        try{
+            ArrayList<String> emptyList = new ArrayList<String>();
+            emptyList.get(0);
+        }catch (Exception e){
+            System.out.println("смоделирован ArrayIndexOutOfBoundsException:");
+            e.printStackTrace();
+        }
+        try{
+            throw new ClassCastException();
+        }catch (Exception e){
+            System.out.println("смоделирован бросок через throw:");
+            e.printStackTrace();
+        }
+    }
 }
