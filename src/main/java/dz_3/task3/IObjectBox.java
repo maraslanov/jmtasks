0a2b9c6f6a09c70d11a8f8c6a1bae2c2f545c65e
MrSmith <hhperm.56@yandex.ru>
2019-11-10 17:00:04 +0500

    dz_3 task3

---
+++ src/main/java/dz_3/task3/IObjectBox.java
@@ -0,0 +1,7 @@
+package dz_3.task3;
+
+public interface IObjectBox<T> {
+    public void addObject(T object);
+    public void deleteObject(T object);
+    public String dump();
+}
--- src/main/java/dz_3/task3/task3.java
+++ src/main/java/dz_3/task3/task3.java
@@ -1,6 +1,6 @@
-package dz_3.task1;
+package dz_3.task3;
 
-public class task1 {
+public class task3 {
     public static void main(String[] args) {
         Number[] arrNumbers = new Number[]{0, 10, 20L, 20, 20D, 20.7};
         MathBox mathBox = new MathBox(arrNumbers);
@@ -10,19 +10,27 @@ public class task1 {
         System.out.println(mathBox.summator());
 
         System.out.println("коллекция с типами до удаления Integer");
-        for (Number number : mathBox.getNumberSet()) {//Number или Object?
+        for (Object number : mathBox.getSet()) {
             System.out.println(number.getClass() + " " + String.valueOf(number));
         }
 
         System.out.println("коллекция с типами после удаления Integer");
         mathBox.deleteAllIntegerElements();
-        for (Number number : mathBox.getNumberSet()) {
+        for (Object number : mathBox.getSet()) {
             System.out.println(number.getClass() + " " + String.valueOf(number));
         }
 
         int delitel = 2;
         System.out.println("Деление всех элементов на "+delitel);
         mathBox.splitter(delitel);
+        System.out.println(mathBox.toString());
+
+        System.out.println("Добавлен элемент Long 123 ");
+        mathBox.addObject(new Long(123));
+        System.out.println(mathBox.toString());
+
+        System.out.println("Удален элемент Long 123 ");
+        mathBox.deleteObject(123L);
         System.out.println(mathBox.toString());
     }
 }
